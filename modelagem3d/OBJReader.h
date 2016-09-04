f4af46d9f15811c45c6ecc71ab7c968a837f713d
Wesley F <Wesley F>
2016-09-04 12:10:25 -0300

    WIP ObjecReader

--- modelagem3d/OBJReader.h
+++ modelagem3d/OBJReader.h
@@ -7,17 +7,44 @@
 
 using namespace std;
 
+char comentarioChar = '#';
+
+vector<string> split(string str, char delimiter) {
+	vector<string> internal;
+	stringstream ss(str);
+	string tok;
+	while (getline(ss, tok, delimiter)) {
+		internal.push_back(tok);
+	}
+	return internal;
+}
+
+bool isComentario(vector<string> sLine) {
+	return sLine[0][0] == comentarioChar;
+}
+
+Mesh* buildMesh(ifstream *file) {
+	string line;
+	while (getline(*file, line)) {
+		vector<string> sLine = split(line, ' ');
+		if (sLine.size() > 0) {
+			if (!line.empty() && !isComentario(sLine)) {
+				for (string text : sLine) {
+					cout << text;
+					cout << "\n";
+				}
+			}
+		}
+	}
+	return 0;
+}
+
 Mesh* readObjFile(string filename) {
 	ifstream file(filename);
 	if (file.fail()) {
 		return 0;
-	}
-	else {
-		string line;
-		while (getline(file, line)) {
-			printf("%s", line);
-		}
-		return 0;
+	} else {
+		return buildMesh(&file);
 	}
 }
 
